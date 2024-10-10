c7fafa694565981895bbef833bc48d392bb9bc19
soodala2003 <soodala78@gmail.com>
2024-10-10 11:24:56 -0400

    updated index.js file

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const counter = 10000;
+const result = 0;
 
 
 function isPrime(n, divisor) {
-    if (n === 1) {
-        return false;
-    } else if (n === 2) {
-        return true;
-    } else if (n % divisor === 0) {
-        return false;
-    } else if (divisor ** 2 > n) {
-        return true;
-    } else {
-        return isPrime(n, divisor + 1);
+    for (let i = 2; i <= n; i++) {
+        let isPrime = true;
+        if (i < 2) {
+            return isPrime = false;
+        } else if (i === 2) {
+            return isPrime = true;
+        } else {
+            if (i % divisor === 0) {
+                return isPrime = false;
+            } else if (divisor ** 2 > i) {
+                return isPrime = true;
+            } else {
+                return isPrime(i, divisor + 1);
+            }
+        }       
     }
 }
+
+let isPrime = true;
+
+try {
+    if (isPrime(counter, divisor)) {
+        console.log(n);          
+    } else {
+        throw "Error!";
+    } 
+} catch (err) {
+    console.log(err);
+    console.log("This catch block more code if the 'try' block fails!");
+} finally {
+    console.log(counter);
+}
+
 
 
