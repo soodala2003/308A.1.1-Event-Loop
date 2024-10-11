db0a9905a27e6de48bcdcc18855a5f08a9a4272d
soodala2003 <soodala78@gmail.com>
2024-10-11 00:59:50 -0400

    updated index.js and index.html files

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-const factorial = (n) => { // function factorial(n)
-    if (n === 0) return 1; // The base case, to stop recursion
-    return n * factorial(n - 1); // The recursive call
-    //return n ? n * factorial(n -1) : 1 
+const heading = document.querySelector("h1");
+const textEl = document.querySelector("p");
+const num = 10000;
+
+heading.innerText = `Prime numbers between 1 and ${num} :`; 
+textEl.innerText = primeLists(num);
+
+function isPrime(n, divisor) {
+    try {
+        if (n < 2) {
+            return false;
+        } else if (n === 2) {
+            return true;
+        } else if (n % divisor === 0) {
+            return false;
+        } else if (divisor ** 2 > n) {
+            return true;
+        } else {
+            return isPrime(n, divisor + 1);
+        }  
+    } catch (err) {
+        console.log(err);
+        console.log(n);
+    }
+} 
+    
+function primeLists(n) {
+    let primeNumArr = [];
+    let divisor = 2;
+    for (let i = 1; i <= n; i++) {
+        if (isPrime(i, divisor) === true) {
+            primeNumArr.push(i);
+        } 
+    }
+    alert("Calaulation is Done!");
+    return primeNumArr;
+} 
+
+const factorial = (n) => { 
+    if (n === 0) {
+        return 1;                  
+    } else {    
+        return n * factorial(n - 1); 
+    }
 }
 
 const facto = (n, a = 1) => {
-    if (n === 0) return a;
-    return () => facto(n - 1, n * a);
+    if (n === 0) {
+        return a;
+    } else {
+        return () => facto(n - 1, n * a);
+    }
 }
-
-function newFactorial (n) {
-    let facto = function myFactorial(n, a) {
-        return n ? myFactorial(n * a, n -1) : a
-    };
-    return facto(n, 1)
-}
-
-facto(10000, 1);
 
 const trampoline2 = (f, ...args) => {
     let result = f(...args);
@@ -26,105 +60,4 @@ const trampoline2 = (f, ...args) => {
     return result;
 }
 
-console.log(trampoline2(facto(10000)));
-
-
-
-/* function flatten(arr) {
-    let result = []; // This will hold the flattened result
-    for (const item of arr) { // Loop through each item in the array
-        if (Array.isArray(item)) { // Check if the item is an array
-            //console.log(item);
-            result = result.concat(flatten(item)); // Call flatten recursively and combine results
-            //console.log(result);
-        } else {
-            result.push(item); // If not an array, add the item to the result
-        }
-    }
-    return result; // Return the flattened array
-}
-   
-// Usage
-const nestedArray = [1, [2, [3, [4, [5]]]], 6];
-const flatArray = flatten(nestedArray);
-console.log(flatArray); // Output: [1, 2, 3, 4, 5, 6]
-
-try {
-    if (flatten(nestedArray)) {
-        console.log(flatArray);          
-    } else {
-        throw "Error!";
-    } 
-} catch (err) {
-    console.log(err);
-    console.log("This catch block more code if the 'try' block fails!");
-} finally {
-    console.log(flatArray);
-} 
-
-const trampoline = (f, ...args) => {
-    let result = f(...args);
-    while (typeof result === "function") {
-      result = result();
-    }
-    return result;
-}
-
-
- */
-
-/* const counter = 15000;
-
-function primeNumber(n, divisor) {
-    let isPrime = true;
-    if (n < 2) {
-        return isPrime = false;
-    } else if (n === 2) {
-        isPrime = true;
-        //console.log(n);
-         return isPrime;
-    } else {
-        if (n % divisor === 0) {
-            isPrime = false;
-            return isPrime;
-        } else if (divisor ** 2 > n) {
-            isPrime = true;
-            //console.log(n);
-            return isPrime;
-        } else {
-            return primeNumber(n, divisor + 1);
-        }   
-    }  
-          
-}
-
-console.log(primeNumber(10, 2));
-
-
-for (let i = 2; i <= counter; i++) {
-    try {
-        if (primeNumber(i, 2)) {
-            //console.log(`${i} is Prime!`);          
-        } else {
-            throw "Error!";
-        } 
-    } catch (err) {
-        console.log(err);
-        console.log(counter);
-    } finally {
-        console.log(counter);
-    }
-}
-
-
-
-
-
- */
-
-
-
-
-
-
-
+console.log(trampoline2(facto(10000))); 
